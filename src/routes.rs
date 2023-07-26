17bd58afd18514dfd08d7c7e8497730e7654223e
Artem Korobko <korobko@amazon.com>
2023-07-26 14:33:13 +0200

    Test that root path returns 404

--- src/routes.rs
+++ src/routes.rs
@@ -5,3 +5,28 @@ mod health;
 pub fn build() -> Router {
     Router::new().merge(health::route())
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::{Body, HttpBody},
+        http,
+    };
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn root_path_returns_404() {
+        let request = http::Request::builder()
+            .uri("/")
+            .method(http::Method::GET)
+            .body(Body::empty())
+            .unwrap();
+
+        let mut response = build().oneshot(request).await.unwrap();
+
+        assert_eq!(response.status(), http::StatusCode::NOT_FOUND);
+        assert!(response.data().await.is_none());
+    }
+}
