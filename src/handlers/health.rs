b1f5661a39381c89703e4f3b13d3f3c14b265124
Artem Korobko <korobko@amazon.com>
2023-08-02 17:55:25 +0200

    Move health endpoint test to handler module

--- src/handlers/health.rs
+++ src/handlers/health.rs
@@ -19,3 +19,33 @@ impl<'a> Response<'a> {
 pub async fn handle() -> impl IntoResponse {
     Json(Response::from_env())
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::{Body, HttpBody},
+        http,
+    };
+    use tower::ServiceExt;
+
+    use crate::{database, routes};
+
+    #[tokio::test]
+    async fn return_service_info() {
+        let request = http::Request::builder()
+            .uri("/health")
+            .method(http::Method::GET)
+            .body(Body::empty())
+            .unwrap();
+
+        let mut response = routes::build(database::Users::default())
+            .oneshot(request)
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), http::StatusCode::OK);
+        let response_body = response.data().await.unwrap().unwrap();
+        let expected_body = r#"{"service":"axum-demo","version":"0.1.0"}"#;
+        assert_eq!(response_body, expected_body);
+    }
+}
--- src/routes/health.rs
+++ src/routes/health.rs
@@ -5,30 +5,3 @@ use crate::handlers;
 pub fn route() -> Router {
     Router::new().route("/health", routing::get(handlers::health::handle))
 }
-
-#[cfg(test)]
-mod tests {
-    use axum::{
-        body::{Body, HttpBody},
-        http,
-    };
-    use tower::ServiceExt;
-
-    use super::*;
-
-    #[tokio::test]
-    async fn return_service_info() {
-        let request = http::Request::builder()
-            .uri("/health")
-            .method(http::Method::GET)
-            .body(Body::empty())
-            .unwrap();
-
-        let mut response = route().oneshot(request).await.unwrap();
-
-        assert_eq!(response.status(), http::StatusCode::OK);
-        let response_body = response.data().await.unwrap().unwrap();
-        let expected_body = r#"{"service":"axum-demo","version":"0.1.0"}"#;
-        assert_eq!(response_body, expected_body);
-    }
-}
