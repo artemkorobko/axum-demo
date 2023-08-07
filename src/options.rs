b4e279b9e9f0d8d78db6d308ef898d27551172fb
Artem Korobko <korobko@amazon.com>
2023-08-07 13:01:21 +0200

    Add test for parsing empty cmd

--- src/options.rs
+++ src/options.rs
@@ -13,3 +13,25 @@ pub struct Options {
     #[arg(short, long, default_value = "100")]
     pub users: usize,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::net::Ipv4Addr;
+
+    use clap::Parser;
+
+    use super::*;
+
+    const APP_BINARY_NAME: &str = "app";
+
+    #[test]
+    fn parse_no_arguments() {
+        let cmd = &[APP_BINARY_NAME];
+
+        let options = Options::try_parse_from(cmd).expect("Failed to parse empty arguments");
+
+        assert_eq!(options.ip, Ipv4Addr::new(0, 0, 0, 0));
+        assert_eq!(options.port, 8080);
+        assert_eq!(options.users, 100);
+    }
+}
